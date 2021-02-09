813c8db6e3204942feac428b4c560a82fb66de4b
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-02-09 13:28:55 +0900

    update formタグ

--- sent.php
+++ sent.php
@@ -26,9 +26,15 @@
       <div class="form-item">■ 名前</div>
       <?php echo $_POST["name"]; ?>
 
+      <div class="form-item">■ 年齢</div>
+      <?php echo $_POST["age"]; ?>
+
+      <div class="form-item">■ お問い合わせの種類</div>
+      <?php echo $_POST["category"]?>
+
       <div class="form-item">■ 内容</div>
       <?php echo $_POST["body"]; ?>
-      
+
     </div>
 
   </div>
