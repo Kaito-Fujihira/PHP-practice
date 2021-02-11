3f8c9329790b769e4dd4ace37366c3ba2d14342d
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-02-11 11:11:36 +0900

    add foreach, endforeach

--- index2.php
+++ index2.php
@@ -2,8 +2,8 @@
 class Menu {
   public $name;
   
-  public function __construct() {
-       
+  public function __construct($name) {
+    $this->name = $name;
   }
   
   public function hello() {
@@ -11,12 +11,32 @@ class Menu {
   }
 }
 
-$curry = new Menu();
-$pasta = new Menu();
-$curry->name = 'CURRY';
-$pasta->name = 'PASTA';
-$curry->hello();
-echo '<br>';
-$pasta->hello();
+$juice = new Menu("JUICE");
+$coffee = new Menu("COFFEE");
+$curry = new Menu("CURRY");
+$pasta = new Menu("PASTA");
+$curry->name = "CURRY";
+$pasta->name = "PASTA";
+$manus = array($juice, $coffee, $curry, $pasta);
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      <?php foreach($menus as $menu): ?>
+        <h3><?php echo $menu->name ?></h3>
+      <?php endforeach ?>
+    </div>
+  </div>
+</body>
+</html>
