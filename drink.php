48e090cf66ffaea78d7a671f6686260742cc2355
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-02-18 12:23:10 +0900

    copy construct of Menu class

--- drink.php
+++ drink.php
@@ -5,6 +5,14 @@ require_once("menu.php");
 class Drink extends Menu {
   private $type;
 
+  public function __construct($name, $price, $image, $type) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->image = $image;
+    self::$count++;
+    $this->type = $type;
+  }
+
   public function getType() {
     return $this->type;
   }
