a3fa049494bb3c61767b5284f0eac9678c2ddda7
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-02-20 08:51:16 +0900

    add

--- food.php
+++ food.php
@@ -3,7 +3,16 @@
 require_once("menu.php");
 
 class Food extends Menu {
-  
+  private $spiciness;
+
+  public function __construct($name, $price, $image, $spiciness) {
+    parent::__construct($name, $price, $image); # 親クラスコンストラクタの呼び出し
+    $this->spiciness = $spiciness;
+  }
+
+  public function getSpiciness() {
+    return $this->spiciness;
+  }
 }
 
 ?>
