82cd85166c21cf946adc80347da1a5c9d09a834f
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-28 12:44:19 +0900

    add lesson10

--- DojoⅠ_index.php
+++ DojoⅠ_index.php
@@ -90,3 +90,23 @@ foreach ($menus as $menu) {
   echo "<br>";
 }
 ?>
+
+
+<!-- lesson10 -->
+<?php
+$menus = array(
+  array("name" => "CURRY", "price" => 900),
+  array("name", => "PASTA", "price" => 1200),
+  array("name" => "COFFEE", "price" => 600)
+);
+
+
+$totalPrice = 0;
+foreach ($menus as $menu) {
+  echo $menu["name"]."は".$menu["price"]."円です";
+  echo "<br>";
+  $totalPrice += $menu["price"];
+}
+echo "合計金額は".$totalPrice."円です";
+
+?>
