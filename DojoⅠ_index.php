cd2319cb716f4aac62091c66f4c03562e5c7ee72
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-29 14:04:25 +0900

    add lesson11

--- DojoⅠ_index.php
+++ DojoⅠ_index.php
@@ -100,7 +100,6 @@ $menus = array(
   array("name" => "COFFEE", "price" => 600)
 );
 
-
 $totalPrice = 0;
 foreach ($menus as $menu) {
   echo $menu["name"]."は".$menu["price"]."円です";
@@ -108,5 +107,30 @@ foreach ($menus as $menu) {
   $totalPrice += $menu["price"];
 }
 echo "合計金額は".$totalPrice."円です";
+?>
 
+
+<!-- lesson11 -->
+<?php
+$menus = array(
+  array("name" => "CURRY", "price" => 900),
+  array("name" => "PASTA", "price" => 1200),
+  array("name" => "COFFEE", "price" => 600)
+);
+
+$totalPrice = 0;
+$maxPrice = 0;
+$maxPriceName = "";
+foreach ($menus as $menu) {
+  echo $menu["name"]."は".$menu["price"]."円です";
+  echo "<br>"
+  $totalPrice += $menu["price"];
+  if ($maxPrice < $$menu["price"]) {
+    $maxPrice = $menu["price"];
+    $maxPriceName = $menu["name"]
+  }
+}
+echo "合計金額は".$totalPrice."円です";
+echo "<br>"
+echo $maxPriceName."が最高価格で".$maxPrice."円です";
 ?>
